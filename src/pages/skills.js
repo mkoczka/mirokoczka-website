06d63dad7ff2d0d880b02591538dce92d2655970
Miro Koczka <miroslav.koczka@pipelinersales.com>
2017-08-14 17:51:02 +0200

    simplify skills page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,47 +2,21 @@ import React from 'react'
 
 export default () =>
   <div>
-    <div className="row">
-      <div className="col-lg-3 col-12">
-        <h3>Languages</h3>
-        <ul>
-          <li>html5</li>
-          <li>css (scss, less)</li>
-          <li>javascript (es6)</li>
-          <li>typescript</li>
-        </ul>
-      </div>
-      <div className="col-lg-3 col-12">
-        <h4>Methods & Tools</h4>
-        <ul>
-          <li>redux</li>
-          <li>graphql, rest</li>
-          <li>bem</li>
-          <li>npm, webpack, gulp</li>
-          <li>pwa (progressive web apps)</li>
-          <li>amp</li>
-        </ul>
-      </div>
-      <div className="col-lg-3 col-12">
-        <h4>Frameworks</h4>
-        <ul>
-          <li>react</li>
-          <li>angular 2</li>
-          <li>ember</li>
-          <li>expressjs</li>
-          <li>meteor</li>
-          <li>graphql</li>
-        </ul>
-      </div>
-      <div className="col-lg-3 col-12">
-        <h4>Additional</h4>
-        <ul>
-          <li>git, svn</li>
-          <li>docker</li>
-          <li>jira, trello</li>
-          <li>photoshop, illustrator</li>
-          <li>scrum, agile</li>
-        </ul>
-      </div>
-    </div>
+    <h3>Frameworks</h3>
+    <p>angular 2/ionic/react/react native/ember/expressjs/meteor/graphql</p>
+    <br/>
+    <br/>
+
+    <h3>Languages</h3>
+    <p>javascript (es6)/typescript/html5/css/scss/less</p>
+    <br/>
+    <br/>
+
+    <h3>Methods & Tools</h3>
+    <p>redux/graphql/rest/bem/npm/webpack/gulp/pwa (progressive web apps)/amp</p>
+    <br/>
+    <br/>
+
+    <h3>Additional</h3>
+    <p>git/svn/docker/jira/trello/photoshop/illustrator/scrum/agile</p>
   </div>
